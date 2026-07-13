8a2501054969394fa79b90bcfc11a441ca748bee
RAGHU RANJAN NAYAK <raghuranjannayak01@gmail.com>
2026-07-13 22:16:30 +0530

    Traffic light programm

---
+++ 02_Conditional_Statement/01_If_Elif_And_Else.py
@@ -0,0 +1,12 @@
+#Traffic light programm
+light = input("Enter colour of light:")
+
+if(light == "red"):
+    print("STOP")
+elif(light == "yellow"):
+    print("LOOK")
+elif(light == "green"):
+    print("GO")
+else:
+    print("LIGHT IS BROKEN")
+    
