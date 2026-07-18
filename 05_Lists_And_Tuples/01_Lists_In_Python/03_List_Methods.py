9b04fdad0d3953c70a84e4f0088203f59e14c0dd
RAGHU RANJAN NAYAK <raghuranjannayak01@gmail.com>
2026-07-18 11:18:31 +0530

    List methods

---
+++ 05_Lists_And_Tuples/01_Lists_In_Python/03_List_Methods.py
@@ -0,0 +1,30 @@
+#Methods in list
+list = [2,1,3]
+
+#Append
+list.append(4)
+print(list)
+
+#Sort
+list.sort()
+print(list)
+
+#Reverse sort
+list.sort(reverse = True)
+print(list)
+
+#Reverse
+list.reverse()
+print(list)
+
+#Insert element at index
+list.insert(4,5)
+print(list)
+
+#Remove element
+list.remove(5)
+print(list)
+
+#Remove element at index
+list.pop(3)
+print(list)
