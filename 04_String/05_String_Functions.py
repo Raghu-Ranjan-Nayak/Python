d4acebb374bf8e15805b359309f38f47471857fb
RAGHU RANJAN NAYAK <raghuranjannayak01@gmail.com>
2026-07-18 11:59:54 +0530

    String functions

---
+++ 04_String/05_String_Functions.py
@@ -0,0 +1,18 @@
+#Methods in string
+str = "raghu is a backend developer."
+
+#endswith
+print(str.endswith("er."))
+
+#capitalize
+print(str.capitalize())
+
+#replace string
+print(str.replace("a","o"))
+
+#find string
+print(str.find("o"))
+
+#count string
+print(str.count("a"))
+
