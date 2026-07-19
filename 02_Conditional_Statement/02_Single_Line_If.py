170e700ba96b3c8d187a69c4c153221275bc6df8
RAGHU RANJAN NAYAK <raghuranjannayak01@gmail.com>
2026-07-19 14:27:17 +0530

    Delete 02_Conditional_Statement folder

--- 02_Conditional_Statement/02_Single_Line_If.py
+++
@@ -1,3 +0,0 @@
-#Is boy Adult or not
-age = int(input("Enter age of a boy:"))
-print("Adult") if age >= 18 else print("Not adult")
--- 02_Conditional_Statement/03_Clever_If.py
+++
@@ -1,4 +0,0 @@
-#Is boy abule to vote by using clever if conditional statement
-age = int(input("Enter age of a boy:"))
-vote = ("no","yes")[age >= 18]
-print(vote)
