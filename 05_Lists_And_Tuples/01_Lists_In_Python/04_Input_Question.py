3aa821cd9b0686027a9529ca072e0b2078e49886
RAGHU RANJAN NAYAK <raghuranjannayak01@gmail.com>
2026-07-19 12:24:23 +0530

    Input movies from user and store in list

---
+++ 05_Lists_And_Tuples/01_Lists_In_Python/04_Input_Question.py
@@ -0,0 +1,9 @@
+#Taka input from user that enter your 3 favorite movies
+movies = []
+
+movies.append(input("Enter 1st movie:"))
+movies.append(input("Enter 2nd movie:"))
+movies.append(input("Enter 3rd movie:"))
+
+print(movies)
+
