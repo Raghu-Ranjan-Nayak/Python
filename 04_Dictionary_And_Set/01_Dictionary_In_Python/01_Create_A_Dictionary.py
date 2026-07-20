9ee5d4fcc9ea6bcaa7b4433139b8c357a7f991fe
RAGHU RANJAN NAYAK <raghuranjannayak01@gmail.com>
2026-07-20 09:32:32 +0530

    Create a dictionary

---
+++ 04_Dictionary_And_Set/01_Dictionary_In_Python/01_Create_A_Dictionary.py
@@ -0,0 +1,11 @@
+#This is for creating a dictionary in python
+dict = {
+    "name" : "Raghu",
+    "cgpa" : 9.0,
+    "marks" : [90,91,95]
+}
+#This is possible for muatable
+dict["name"] = "Raja"
+
+dict["surname"] = "Nayak"
+print(dict)
