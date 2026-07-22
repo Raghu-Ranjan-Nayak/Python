a6e25780757b2b3854dfaaa9301f69dc21a17dc1
RAGHU RANJAN NAYAK <raghuranjannayak01@gmail.com>
2026-07-22 12:06:26 +0530

    Range function in python

---
+++ 05_Loops_In_Python/05_Range_Fun_In_Loop.py
@@ -0,0 +1,14 @@
+#Range function in loop
+
+for i in range(10):#range(stop)
+    print(i)
+
+print("\n")
+
+for i in range(2,10):#range(start,stop)
+    print(i)
+
+print("\n")
+
+for i in range(2,10,2):#range(start,stop,step)
+    print(i)
