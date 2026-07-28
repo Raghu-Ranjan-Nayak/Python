1c5f76e1c5388776e5860c72134ca56b3a501978
RAGHU RANJAN NAYAK <raghuranjannayak01@gmail.com>
2026-07-28 12:38:21 +0530

    Find line

--- 07_File_Input_&_Output/06_Question.py
+++ 07_File_Input_&_Output/06_Question.py
@@ -14,11 +14,25 @@
 # with open("07_File_Input_&_Output/Practice.txt","w") as f:
 #     f.write(new_data)
 
-#Write a programm which find the word learning in the paragraph or not
-with open("07_File_Input_&_Output/Practice.txt","r") as f:
-    data = f.read()
-    if(data.find("learning") != -1):
-        print("Found")
-    else:
-        print("Not Found")
+# #Write a programm which find the word learning in the paragraph or not
+# with open("07_File_Input_&_Output/Practice.txt","r") as f:
+#     data = f.read()
+#     if(data.find("learning") != -1):
+#         print("Found")
+#     else:
+#         print("Not Found")
+
+#Write a function which find the line inwhich learning woard is exist
+def Check_the_line():
+    word ="Learning"
+    line_no = 1
+    data = True
+    with open("07_File_Input_&_Output/Practice.txt","r") as f:
+        while data:
+            data = f.readline()
+            if(word in data):
+                print(line_no)
+            line_no += 1
+
+
 
