377a93f37dc06ac0556d70dd4000a190cb8aa3ae
RAGHU RANJAN NAYAK <raghuranjannayak01@gmail.com>
2026-08-17 23:15:06 +0530

    use properties and methods of car class by inheritance

---
+++ 08_Oops/06_Inheritance.py
@@ -0,0 +1,15 @@
+#Use properties and methods of a car class by inheritance
+class Car:
+    @staticmethod
+    def start():
+        print("car started")
+    @staticmethod
+    def stop():
+        print("car stoped")
+class Toyotacar(Car):
+    def __init__(self,name):
+        self.name = name
+car1 = Toyotacar("fortuner")
+car2 = Toyotacar("prius")
+
+print(car1.start())
