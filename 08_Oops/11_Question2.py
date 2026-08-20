ea86431bfe532b10f6b1819b343aa4e81769d2a8
RAGHU RANJAN NAYAK <raghuranjannayak01@gmail.com>
2026-08-20 20:11:55 +0530

    Engineer class is inheritant the employee class

--- 08_Oops/11_Question2.py
+++ 08_Oops/11_Question2.py
@@ -13,7 +13,8 @@ class Engineer(Employee):
     def __init__(self,name,age):
         self.name = name
         self.age = age
+        super().__init__("devloper","software","12 LPA")
 
 
-e1 = Employee("devloper","software","12 LPA")
+e1 = Engineer("Raghu",19)
 e1.showDetails()
