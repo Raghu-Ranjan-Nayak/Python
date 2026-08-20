a7f406245a436ed9bff90288e58240910124ab6c
RAGHU RANJAN NAYAK <raghuranjannayak01@gmail.com>
2026-08-20 20:06:36 +0530

    Engineer class is inheritant the employee class

---
+++ 08_Oops/11_Question2.py
@@ -0,0 +1,19 @@
+#Engineer class is inheritant the employee class
+class Employee:
+    def __init__(self,roll,department,salary):
+        self.role = roll
+        self.department = department
+        self.salary = salary
+    def showDetails(self):
+        print("Roll is",self.role)
+        print("Department is",self.department)
+        print("Salary is",self.salary)
+
+class Engineer(Employee):
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+
+e1 = Employee("devloper","software","12 LPA")
+e1.showDetails()
